0ee66994facec528934c9bfaabe7f236b8bf14c6
Starky-16 <164493610+Starky-16@users.noreply.github.com>
2025-11-30 10:28:32 +0530

    Delete imu_sensor.py

--- imu_sensor.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class IMUSensor:
-    def __init__(self):
-        pass
-
-    def get_angle(self):
-        # Simulate tilt angle (replace with ROS topic subscription in real case)
-        return random.uniform(-5, 5)
